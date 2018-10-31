ee5f7642dd6933e6e5503dc188b3e407e0a103fe
Victor Nicacio <39224838+Enki-N@users.noreply.github.com>
2018-10-31 12:36:24 -0300

    Update listadeprodutos.c

--- listadeprodutos.c
+++ listadeprodutos.c
@@ -2,29 +2,49 @@
 
 PLISTA criarLista(){
   PLISTA res = (PLISTA) malloc(sizeof(LISTADEPRODUTOS));
-  res->cabeca = (PONT) malloc(sizeof(REGISTRO));
-  res->cabeca->id=-1;
-  res->cabeca->tipo=-1;
-  res->cabeca->quantidade=0;
-  res->cabeca->valorUnitario=0;
-  res->cabeca->proxProd=NULL;
+  int x;
+  for (x=0;x<NUMTIPOS;x++){
+    res->LISTADELISTAS[x]=(PONT) malloc(sizeof(REGISTRO));
+    res->LISTADELISTAS[x]->id=-1;
+    res->LISTADELISTAS[x]->quantidade=0;
+    res->LISTADELISTAS[x]->valorUnitario=0;
+    res->LISTADELISTAS[x]->proxProd=NULL;
+  }
   return res;
 }
 
 
 int tamanho(PLISTA l){
-  PONT atual = l->cabeca->proxProd;
   int tam = 0;
-  while (atual) {
-    atual = atual->proxProd;
-    tam++;
+  int x;
+  PONT atual;
+  for (x=0;x<NUMTIPOS;x++){
+    atual = l->LISTADELISTAS[x]->proxProd;
+    while (atual) {
+      atual = atual->proxProd;
+      tam++;
+    }
   }
   return tam;
 }
 
 PONT buscarID(PLISTA l, int id){
-  PONT atual = l->cabeca->proxProd;
-   while (atual) {
+  int x;
+  PONT atual;
+  for (x=0;x<NUMTIPOS;x++){
+    atual = l->LISTADELISTAS[x]->proxProd;
+    while (atual) {
+      if (atual->id == id) return atual;
+      atual = atual->proxProd;
+    }
+  }
+  return NULL;
+}
+
+PONT buscarIDTipo(PLISTA l, int id, int tipo){
+  if (tipo<0 || tipo>=NUMTIPOS) return NULL;
+  PONT atual = l->LISTADELISTAS[tipo]->proxProd;
+  while (atual) {
     if (atual->id == id) return atual;
     atual = atual->proxProd;
   }
@@ -33,127 +53,109 @@ PONT buscarID(PLISTA l, int id){
 
 void exibirLog(PLISTA f){
   int numElementos = tamanho(f);
-  printf("Log lista [elementos: %i]\n    ", numElementos);
-  PONT atual = f->cabeca->proxProd;
-  while (atual){
-    printf(" [%i;%i;%i;%i;$%i]", atual->id, atual->tipo, atual->quantidade, atual->valorUnitario, atual->quantidade*atual->valorUnitario);
-    atual = atual->proxProd;
+  printf("Log lista [elementos: %i]\n", numElementos);
+  int x;
+  PONT atual;
+  for (x=0;x<NUMTIPOS;x++){
+    printf("  #TIPO: %i -> ", x);
+    atual = f->LISTADELISTAS[x]->proxProd;
+    while (atual){
+      printf(" [%i;%i;%i;$%i]", atual->id, atual->quantidade, atual->valorUnitario, atual->quantidade*atual->valorUnitario);
+      atual = atual->proxProd;
+    }
+    printf("\n");
   }
-  printf("\n\n");
+  printf("\n");
 }
 
 int consultarValorUnitario(PLISTA l, int id){
-  PONT atual = l->cabeca->proxProd;
-   while (atual) {
-    if (atual->id == id) return atual->valorUnitario;
-    atual = atual->proxProd;
+  int x;
+  PONT atual;
+  for (x=0;x<NUMTIPOS;x++){
+    atual = l->LISTADELISTAS[x]->proxProd;
+    while (atual) {
+      if (atual->id == id) return atual->valorUnitario;
+      atual = atual->proxProd;
+    }
   }
   return 0;
 }
 
-/* Busca um REGISTRO pelo valor total */
-PONT buscaValorTotalIns(PLISTA l, int valorTotal, PONT* ant){
-  *ant = l->cabeca;
-  PONT atual = l->cabeca->proxProd;
+PONT buscaValorTotalINS(PLISTA l, int tipo, int valorTotal, PONT* ant){
+  PONT atual;
+  int i;
 
-  while(atual && atual->quantidade * atual->valorUnitario < valorTotal){
-    if(atual && atual->quantidade * atual->valorUnitario == valorTotal) return atual;
-    *ant = atual;
-    atual = atual->proxProd;
-  }
+  *ant = l -> LISTADELISTAS[tipo];
+  atual = l -> LISTADELISTAS[tipo] -> proxProd;
+
+    while(atual){
+      if(atual -> quantidade * atual -> valorUnitario >= valorTotal) return atual;
+      *ant = atual;
+      atual = atual -> proxProd;
+    }
+
   return NULL;
 }
 
-/* Busca um REGISTRO, mas passando por parãmetro o ponteiro para o REGISTRO anterior */
-PONT buscarReg(PLISTA l, int id, PONT* ant){
-  *ant = l->cabeca;
-  PONT atual = l->cabeca->proxProd;
-   while (atual) {
-    if (atual->id == id) return atual;
-    *ant = atual;
-    atual = atual->proxProd;
-  }
-  return NULL;
-}
 
 
 bool inserirNovoProduto(PLISTA l, int id, int tipo, int quantidade, int valor){
-  PONT x, ant;
+  PONT novo, ant, aux, atual;
 
-  /* Teste de validade dos valores recebidos */
-  if(id <= 0 || tipo <= 0 || quantidade <= 0 || valor <= 0) return false;
+  /* Testa se os valores recebidos são positivos e se o TIPO esta no intervalo correto */
+  if(id <= 0 || quantidade <= 0 || valor <= 0 || tipo < 0 || tipo > NUMTIPOS-1) return false;
 
-  /* Busca do produto pelo ID */
-  x = buscarID(l, id);
-  if(x != NULL) return false; // Retorna o PONT caso ja exista um produto com o mesmo ID.
+  // Busca o id e analisa se ja existe um produto com o mesmo id
+  novo = buscarID(l, id);
+  if(novo != NULL) return false;
 
-  /* Busca da posição certa do REGISTRO */
-  x = buscaValorTotalIns(l, quantidade*valor, &ant);
-  if(x != NULL) return false;
+  /* Busca a posição certa do REGISTRO */
+  novo = buscaValorTotalINS(l, tipo, quantidade*valor, &ant);
 
-  /* Posicionando os valores e ajustando os ponteiros envolvidos */
-  x = (PONT) malloc(sizeof(REGISTRO));
-  x -> id = id;
-  x -> tipo = tipo;
-  x -> quantidade = quantidade;
-  x -> valorUnitario = valor;
-  x -> proxProd = ant -> proxProd;
-  ant -> proxProd = x;
+  /* Aloca os valores do REGISTRO NOVO */
+  atual = (PONT) malloc(sizeof(REGISTRO));
+  atual -> id = id;
+  atual -> quantidade = quantidade;
+  atual -> valorUnitario = valor;
+
+  if(novo!=NULL){
+    atual -> proxProd = ant -> proxProd;
+    ant -> proxProd = atual;}
+  else{
+    atual->proxProd = NULL;
+    ant->proxProd=atual;
+  }
 
   return true;
 }
 
+
+
 bool removerItensDeUmProduto(PLISTA l, int id, int quantidade){
-  PONT x, ant;
-  int valor, tipo, quant;
+  PONT apagar, x;
+  int tipo;
 
-  /* Retorna false caso o valor de quantidade recebido seja menor ou igual a zero*/
-  if(quantidade <= 0) return false;
+  /* Testa se os valores recebidos são positivos e se o TIPO esta no intervalo correto */
+  if(id <= 0 || quantidade <= 0) return false;
 
-  /* Retorna false caso não exista produto com o id recebido */
-  x = buscarReg(l, id, &ant);
-  if(x == NULL) return false;
+  // Busca o id e analisa se ja existe um produto com o mesmo id
+  x = buscarID(l, id);
+  if(x != NULL) return false;
 
-  /* Guarda os valores do REGISTRO para a inserção */
-  valor = x->valorUnitario;
-  tipo = x->tipo;
-  quant = x->quantidade - quantidade;
-
-  /* Testa se a quantidade recebida é maior que a quantidade real do registro */
-  if(quantidade > x->quantidade) return false;
-
-  /* Apaga o REGISTRO */
-  ant->proxProd = x->proxProd;
+  apagar->id = x->id;
+  apagar->quantidade = x->quantidade - quantidade;
+  apagar->valorUnitario = x->valorUnitario;
+  apagar->proxProd = x->proxProd;
+  tipo = l ->
   free(x);
 
-  /* Insere o novo RESGITRO na posição certa */
-  inserirNovoProduto(l, id, tipo, quant, valor);
-
-  return true;
+  // Inserir o novo REGISTRO com os valores atualizados inserirNovoProduto(l, )
 }
 
 
 bool atualizarValorDoProduto(PLISTA l, int id, int valor){
-  PONT x, ant;
-  int quant, tipo;
 
-  /* Testa se o valor recebido é positivo */
-  if(valor <= 0) return false;
+  /* COMPLETAR */
 
-  /* Busca o REGISTRO pelo id, alocando o endereço do anterior */
-  x = buscarReg(l, id, &ant);
-  if(x == NULL) return false;
-
-  /* Guarda os valores do REGISTRO para a inserção */
-  quant = x->quantidade;
-  tipo = x->tipo;
-
-  /* Apaga o REGISTRO */
-  ant->proxProd = x->proxProd;
-  free(x);
-
-  /* Faz a inserção do REGISTRO na posição certa */
-  inserirNovoProduto(l, id, tipo, quant, valor);
-  
-  return true;
+  return false;
 }
